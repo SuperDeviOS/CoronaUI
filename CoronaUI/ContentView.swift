273967c9c16ff93d2d4cc767283dee58489102d8
Caique <juliaomartinsch@gmail.com>
2020-05-23 11:29:51 -0300

    Initial Commit

---
+++ CoronaUI/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  CoronaUI
+//
+//  Created by Carlos Henrique Martins on 5/23/20.
+//  Copyright © 2020 Carlos Henrique Martins. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
